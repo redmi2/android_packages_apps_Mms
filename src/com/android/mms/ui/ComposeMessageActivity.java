87e8dbb0801746e7bf224245856a3e5a66ee95b2
yanglv <yanglv@codeaurora.org>
2016-11-03 12:21:56 +0800

    Mms: Fix messaging ANR in MTBF test
    Load owner's contact icon in mail thread by block way cause ANR
    in some situation.
    
    Change loading owner icon by unblock way.
    
    Change-Id: I5edd6dc472b76f7de061869ff7074eeca82e92f1
    CRs-Fixed: 1085339

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -6188,7 +6188,7 @@ public class ComposeMessageActivity extends Activity
     }
 
     private void setSendButtonImage() {
-        Contact contact = Contact.getMe(true);
+        Contact contact = Contact.getMe(false);
         if (sDefaultContactImage == null) {
             sDefaultContactImage = this.getResources().getDrawable(R.drawable.default_avatar);
         }
